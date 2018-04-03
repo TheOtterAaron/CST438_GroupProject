81da3446a204accdb96e8216830117cd6381a099
virustotalop <virustotalop@gmail.com>
2018-04-02 21:04:22 -0700

    Updated expense sheet view php class
    Updated expense sheet view php class. Proof of concept for the frontend.
    More data will have to be gathered later from the TripController but for
    now there are two hardcoded locations for origin and destination.

--- php/ExpenseSheetView.php
+++ php/ExpenseSheetView.php
@@ -2,27 +2,43 @@
 <head>
 </head>
 <body>
-<form id="calculate-mileage" action="ExpenseSheetView.php">
-<button type="Submit">Calculate</button>
+<p>Calculates the distance for the client, methods are currently stubbed in for demonstration purposes</p>
+<form id="calculate-mileage" method="post" action="ExpenseSheetView.php">
+<button type="submit" name="calculate">Calculate</button>
 
 </form>
 <?php
+    if(isset($_POST['calculate']))
+    {
+        renderExpenseSheet();
+    }
+
     function renderExpenseSheet()
     {
-        echo "Miles: " + calculateTrips($sheetId);
+        $sheetId = 0; //To be passed later
+        $calculated = calculateTrips($sheetId);
+        echo("Miles: " . $calculated);
     }
 
     function calculateTrips($sheetId)
     {
+        include('MileageCalculator.php');
+        $calculated = calculateMileage("Disneyland Park", "Fisherman's Wharf, San Francisco, CA");
+        return $calculated;
+        /*This will have to be implemented later but its a proof of concept that at least one
+          trip can be calculated.
+        */
         //To be gathered from the trip database
         //Psuedo code
         /*
+        require 'TripController';
         $totalMiles = 0;
         for(Trip trip : TripController.getTrips())
         {
             totalMiles += findDistance(trip.getMileage())
         }*/
     }
+
 ?>
 </body>
 </html>
