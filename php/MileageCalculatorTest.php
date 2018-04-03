66e1454dd5f781f039b0e7f821d8eb07cc936d85
virustotalop <virustotalop@gmail.com>
2018-04-02 20:45:17 -0700

    Commented MileageCalculatorTest class

--- php/MileageCalculatorTest.php
+++ php/MileageCalculatorTest.php
@@ -5,6 +5,9 @@
     echo("</br>");
     $rounded = round($calculated);
     echo("Rounded Value: " . $rounded);
+    //Rounds to the nearest whole number
+    //Using the same test that is implemented in the java portion of the code
     echo("</br>");
     echo("Test passed: " . ($rounded == 409));
+    //If 0 the test failed if the value is 1 the test passed
 ?>
