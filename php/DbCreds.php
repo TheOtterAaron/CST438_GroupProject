02a20765c4577a7eefa042bfdb7f8faa88309746
Aaron Gordon <adgordon@live.com>
2018-04-17 20:15:42 -0700

    Updated local DbCreds.php

--- php/DbCreds.php
+++ php/DbCreds.php
@@ -3,7 +3,7 @@
 $host     = "localhost";
 $dbname   = "mileage_tracker";
 $username = "root";
-$password = "mrRabbitRocks96";
+$password = "pass123";
 
 $dbCon = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 $dbCon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
